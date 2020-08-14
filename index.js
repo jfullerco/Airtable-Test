1d93601ab5521d3957df653df350aaf58c982089
jfullerco <jonathan@jfuller.co>
2020-08-14 10:09:38 -0400

    1 binaries uploaded

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+airtable.configure({apiKey: 'key7XQ8Is5o6z82BJ' });
