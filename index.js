25c5e9ddc2eb1abe72d48bb0636f60695b8606fe
jfullerco <jonathan@jfuller.co>
2020-08-15 03:32:10 -0400

    2 files modified

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 // Import stylesheets
 import './style.css';
-
+import 'jquery'
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
-const custdiv = document.getElementById('rwCustomers');
+
+
 
 //Airtable
 
@@ -18,9 +19,10 @@ const table = base('Customers');
 
 base('Customers').select({
     view: 'Customer Info'
+    
 }).firstPage(function(err, records) {
     if (err) { console.error(err); return; }
     records.forEach(function(record) {
-        custdiv.innerText(record.get('Company Name'));
+        document.getElementById('rwCustomers').innerText('Retrieved', record.get('Company Name'));
     });
 });
