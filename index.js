871fafee82fc39d1da63735563556fe59f9ea7f6
jfullerco <jonathan@jfuller.co>
2020-09-03 16:42:12 -0400

    1 file modified

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-// Import stylesheets
-import Airtable from 'airtable'
+
+import fetch from 'node-fetch'
 import './style.css';
 
 
@@ -7,20 +7,12 @@ import './style.css';
 const appDiv = document.getElementById('app');
  
 
-//Airtable
 
-
-const Airtable = require('airtable');
-const base = new Airtable({ apiKey: 'key7XQ8Is5o6z82BJ' }).base(
-    'app3v5MjJ1b1OGZ1f'
-);
-
-fetch("https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random", {
+fetch("https://joke3.p.rapidapi.com/v1/joke", {
 	"method": "GET",
 	"headers": {
-		"x-rapidapi-host": "matchilling-chuck-norris-jokes-v1.p.rapidapi.com",
-		"x-rapidapi-key": "6ae9ccd8d4msha612d4fbe46ad0dp157a6ajsn13ad227dfd72",
-		"accept": "application/json"
+		"x-rapidapi-host": "joke3.p.rapidapi.com",
+		"x-rapidapi-key": "6ae9ccd8d4msha612d4fbe46ad0dp157a6ajsn13ad227dfd72"
 	}
 })
 .then(response => {
