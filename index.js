f6be5ac207fac45f5563702c6a2d65503b4a4c97
jfullerco <jonathan@jfuller.co>
2020-09-06 17:46:03 -0400

    2 files modified

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import './style.css';
 
 
 const appDiv = document.getElementById('app');
- 
+const selOption = document.getElementById('cSelect') 
 
 
 fetch("https://tiems-d1ca.restdb.io/rest/clients", {
@@ -15,15 +15,25 @@ fetch("https://tiems-d1ca.restdb.io/rest/clients", {
 	}
 })
 .then(res => res.json())
-.then(data => {
- return listClients(data) }
+//.then(data => console.log(data))
+.then(data => listClients(data))
 
-)
+.then(listClients => data)
+
 function listClients(data) {
   for (var i = 0; i < data.length; i++) {
   //console.log(data[i])
-  var div = document.createElement("div")
-  div.innerHTML = data[i]._id
-  appDiv.appendChild(div)
+  var opt = document.createElement("OPTION")
+  opt.label = data[i].client_name
+  selOption.appendChild(opt)
   }
 }
+
+
+
+//.then(data => {
+// return listClients(data) }
+//)
+
+
+
